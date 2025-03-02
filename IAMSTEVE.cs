aec1232988c12e40e307185fa8291edeee3b17e9
gywe_gyh <ivan.maks0130@gmail.com>
2025-03-02 12:24:26 +0300

    Add files via upload

---
+++ IAMSTEVE.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAMSTEVE : MonoBehaviour
+{
+    public GameObject steve;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(steve);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
